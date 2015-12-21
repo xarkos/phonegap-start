7950ae3c773bdd1517e3c7aebbcc47bc142aa612
Xarkos consulting <gordon_leek@yahoo.com>
2015-12-21 15:21:47 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,22 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+       var xhr = new XMLHttpRequest();
+ xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
+  // Response handlers.
+  xhr.onload = function () {
+     var repos = JSON.parse(xhr.response), i, reposHTML = "";
+     for (i = 0; i < repos.repositories.length; i++) {
+       reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
+     }
+     document.getElementById("allRepos").innerHTML = reposHTML;
+  };
+
+  xhr.onerror = function () {
+     alert('error making the request.');
+  };
+
+xhr.send();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
